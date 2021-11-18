61694f76b69d79b168d90c5962669c20b63bcb91
Roman Romanenko <37461099+roman-romanenko@users.noreply.github.com>
2021-11-18 09:46:57 -0500

    Update wks-install.sh

--- wks-install.sh
+++ wks-install.sh
@@ -24,6 +24,7 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt update 
 sudo apt install -y docker-ce
 sudo usermod -aG docker $USER 
+sudo chmod 666 /var/run/docker.sock
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose 
 sudo chmod +x /usr/local/bin/docker-compose
 docker --version
