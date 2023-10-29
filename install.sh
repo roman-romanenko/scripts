ad8ee97576d1a9fa7125c0b629bde9abe0591497
Roman Romanenko <37461099+xa2099@users.noreply.github.com>
2023-10-29 01:27:09 -0400

    .

--- install.sh
+++ install.sh
@@ -7,42 +7,17 @@ sudo touch /etc/cloud/cloud-init.disabled
 echo "Install Needed Software =========================================================================================="
 sudo apt update
 sudo apt upgrade -y
-sudo apt install xubuntu-core xrdp curl wget software-properties-common apt-transport-https ca-certificates gnupg engrampa copyq flameshot ristretto -y
-sudo apt remove xfce4-screensaver xfce4-power-manager -y
-sudo apt autoremove -y
+sudo apt install xrdp kubuntu-desktop curl wget software-properties-common apt-transport-https ca-certificates gnupg -y
 
 echo "Configure XRDP ==================================================================================================="
 echo xfce4-session > ~/.xsession
 sudo wget /etc/polkit-1/localauthority/ https://raw.githubusercontent.com/xa2099/setup/main/config/45-allow-colord.pkla
 
 echo "Configure Theaming ==============================================================================================="
-
-wget https://github.com/xa2099/setup/raw/main/theming/Tela-circle.tar.xz
-sudo tar -xf Tela-circle.tar.xz -C /usr/share/icons
-rm Tela-circle.tar.xz
-
-wget https://github.com/xa2099/setup/raw/main/theming/Nordic-darker.tar.xz
-sudo tar -xf Nordic-darker.tar.xz -C /usr/share/themes
-rm Nordic-darker.tar.xz
-
-sudo wget /usr/share/backgrounds/xfce https://raw.githubusercontent.com/xa2099/setup/main/theming/Nord-wallpaper.jpg
-xfconf-query -c xfce4-desktop -p $(xfconf-query -c xfce4-desktop -l | grep "workspace0/last-image")  -s /usr/share/backgrounds/xfce/Nord-wallpaper.jpg
-
-mkdir -p ~/.config/gtk-3.0
-wget -P ~/.config/gtk-3.0/ https://raw.githubusercontent.com/xa2099/setup/main/theming/gtk.css
-
 wget https://github.com/xa2099/setup/raw/main/theming/fonts.zip
 sudo mkdir /usr/share/fonts/Microsoft
 sudo unzip fonts.zip -d /usr/share/fonts/Microsoft
 rm fonts.zip
-
-xfconf-query -c xsettings -p /Net/ThemeName              -n -t string -s "Nordic-darker"
-xfconf-query -c xsettings -p /Net/theme                  -n -t string -s "Nordic-darker"
-xfconf-query -c xsettings -p /Net/IconThemeName          -n -t string -s "Tela-circle-dark"
-xfconf-query -c xfce4-panel -p /panels/panel-1/position  -n -t string -s 'p=8;x=1000;y=1000'
-xfconf-query -c xfce4-panel -p /panels/panel-1/size      -n -t int -s 40
-xfconf-query -c xfce4-panel -p /panels/panel-1/icon-size -n -t int -s 38
-xfconf-query -c xfce4-panel -p /panels/panel-2 -r -R
 
 echo "Install Docker ==================================================================================================="
 sudo install -m 0755 -d /etc/apt/keyrings
@@ -87,5 +62,6 @@ rm anyconnect-linux64-4.10.01075-k9.tar.gz
 cd anyconnect-linux64-4.10.01075/vpn
 sudo bash vpn_install.sh
 
+echo ""
 echo "Done"
 echo ""
